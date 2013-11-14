6b5fce2b1ebb7092a20c448acb15cf491dd07d8c
Brahim HAMDOUNI <brahim@hamdouni.com>
2013-11-15 00:22:44 +0100

    auto map database model to view model (do-sql-json)

--- app/app.js
+++ app/app.js
@@ -1,12 +1,19 @@
 function PdvCtrl($scope,$http) {
 	$scope.pdvs = [{ 'id':'1','nom':'Epicerie ANDES'},{ 'id':'2','nom':'Au Marché Conté'},{ 'id':'3','nom':'A.I.R.E.S. '}];
 
-	$http.get('srv/pdv.rsp').
-	success(function(data,status){
-		$scope.status = status;
-		$scope.pdvs = data;
-	}).
-	error(function(data,status){
-		$scope.status = status;
-	});
+	$scope.search = function() {
+		$http({
+			method: 'GET',
+			url: 'srv/pdv.rsp',
+			params: {name: $scope.name}
+		}).
+		success(function(data,status){
+			$scope.status = status;
+			$scope.pdvs = data;
+		}).
+		error(function(data,status){
+			$scope.status = status;
+		});
+	}
+
 }
