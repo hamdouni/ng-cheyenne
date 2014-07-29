32fc808e85b2311b89d4b35a0f714e995f870c2f
hamdouni <brahim@hamdouni.com>
2014-07-29 15:04:54 +0200

    experimentation continue

--- app/htmlControlCases.r
+++ app/htmlControlCases.r
@@ -1,14 +1,28 @@
 rebol []
 
-htmlControlCases: [ 
-	string! {
-		func [name value][
-			rejoin [
-				"<div>"
-				"<label>" name "</label>"
-				"<input type='text' name='" name "' value='" value "'>"
-				"</div>"
+update-form: func [model obj /local name type status value p v html][
+	html: ""
+ 	foreach [name type status value] model [
+? type? value
+		do select [
+			string! [
+				repend html [
+					"<div>"
+						"<label>" name "</label>"
+						"<input type='text' name='" name "' value='" obj/:name "'>"
+					"</div>"
+				]
 			]
-		]
-	}
+			list! [
+				repend html ["<div><label>" name "</label><select>"]
+				foreach [p v] value [
+					repend html ["<option value='" p "'>" v "</option>"]
+				]
+				repend html "</select></div>"
+			]
+		] type
+	]
+?? html
+	print html
 ]
+
